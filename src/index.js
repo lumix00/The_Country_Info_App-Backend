0b1e537a20ef9ea5304e071b90b65c9988cb0893
Lucas Nascimento Silva <lumix00gamer@gmail.com>
2024-10-11 16:51:21 -0300

    cors add

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
 const express = require('express');
-const axios = require('axios')
-const app =  express()
+const axios = require('axios');
+const app =  express();
+const cors = require('cors') ;
 const port = process.env.PORT || 3000;
 
-
+app.use(cors());
 app.get('/AvailableCountries', async (req, resp) => {
     try {
         const response = await axios.get('https://date.nager.at/api/v3/AvailableCountries');
